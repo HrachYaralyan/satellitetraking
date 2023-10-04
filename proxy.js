134c58e9d5fe97eaada60f55ea95be0c8b525653
HrachYaralyan <90770993+HrachYaralyan@users.noreply.github.com>
2023-10-04 17:55:13 +0400

    Update proxy.js
    comment proxy

--- proxy.js
+++ proxy.js
@@ -1,9 +1,9 @@
-const corsAnywhere = require('cors-anywhere');
+// const corsAnywhere = require('cors-anywhere');
 
-const PORT = 8080; // Choose a port number that is not in use
+// const PORT = 8080; // Choose a port number that is not in use
 
-corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-}).listen(PORT, () => {
-  console.log(`CORS Anywhere proxy server is running on port ${PORT}`);
-});
+// corsAnywhere.createServer({
+//   originWhitelist: [], // Allow all origins
+// }).listen(PORT, () => {
+//   console.log(`CORS Anywhere proxy server is running on port ${PORT}`);
+// });
